4dea272222327b2df60abd637d97a263b66ef2bd
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-07-08 23:53:19 +0200

    feat: authenticate

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
 use hyper::{Body, Method, Request, Client};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn post_data() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+
+
     let req = Request::builder()
-        .method(Method::POST)
-        .uri("http://httpbin.org/post")
-        .header("content-type", "application/json")
-        .body(Body::from(r#"{"library":"hyper"}"#))?;
+    .method(Method::POST)
+    .uri("http://173.249.46.156:8080/abunzi_mis_service/api/minCases/addCase")
+    .header("content-type", "application/json")
+    .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1bXVtYWoiLCJleHAiOjE2MjU3OTkwNTksImlhdCI6MTYyNTc4MTA1OX0.V_ZGohPVN9cedowOGaoYRwNMIZpJwVZ5tHw6v0QyliD2bfVn_VQSd-2N1EZRshao_q4szo6RUWj1FKy8WzfuXQ")
+    .body(Body::from(r#"{"library":"hyper"}"#))?;
 
     // We'll send it in a second...
 
     let client = Client::new();
 
+    println!("in method");
     // POST it...
     let resp = client.request(req).await?;
 
-    println!("Response: {}", resp.status());
+    println!("{:?}", resp);
 
     Ok(())
+}
+
+#[tokio::main]
+#[allow(unused_must_use)]
+async fn main()  {
+
+    println!("started posting...");
+    post_data().await;
+    println!("ended posting...");
+    // println!("Response: {}", resp.status());
+
+    // Ok(())
 }
 
