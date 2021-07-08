2196c261b09d3f4bf0d1572729ee436a1b36c8e0
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-07-08 23:27:45 +0200

    new: sample request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::{Body, Method, Request, Client};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri("http://httpbin.org/post")
+        .header("content-type", "application/json")
+        .body(Body::from(r#"{"library":"hyper"}"#))?;
+
+    // We'll send it in a second...
+
+    let client = Client::new();
+
+    // POST it...
+    let resp = client.request(req).await?;
+
+    println!("Response: {}", resp.status());
+
+    Ok(())
 }
+
