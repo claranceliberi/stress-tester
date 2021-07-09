bd732f7e5c256802c1a8b02d80560062d0d1e0f5
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-07-09 02:09:09 +0200

    final

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use hyper::{Body, Method, Request, Client};
-use std::time::{ SystemTime, Instant};
-use std::thread;
 
 async fn post_data(req_body:String) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
-
-    let then = SystemTime::now();
 
     let req = Request::builder()
     .method(Method::POST)
@@ -14,30 +10,15 @@ async fn post_data(req_body:String) -> Result<(), Box<dyn std::error::Error + Se
     .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1bXVtYWoiLCJleHAiOjE2MjU3OTkwNTksImlhdCI6MTYyNTc4MTA1OX0.V_ZGohPVN9cedowOGaoYRwNMIZpJwVZ5tHw6v0QyliD2bfVn_VQSd-2N1EZRshao_q4szo6RUWj1FKy8WzfuXQ")
     .body(Body::from(req_body))?;
 
-
     // We'll send it in a second...
 
     let client = Client::new();
 
-    // POST it...
-    // for x in 1..100000 as u128{
-        // let now = SystemTime::now();
-        // println!("{} time : ", x);
-        let resp = client.request(req).await?;
-        println!("{:?} {:?}", resp.status(), Instant::now());
-    // }
-
     
-    match then.elapsed() {
-        Ok(elapsed) => {
-            // it prints '2'
-            println!(" elapsed:{:?}", elapsed);
-        }
-        Err(e) => {
-            // an error occurred!
-            println!("Error: {:?}", e);
-        }
-    }
+    // POST it...
+    let resp = client.request(req).await?;
+
+    println!("{:?} {:?}", resp.status(),resp.headers());
 
     Ok(())
 }
@@ -95,8 +76,10 @@ async fn main()  {
     }
     "#;
 
-    
+    for x in 1..100000{
+        println!("{} time : ", x);
         post_data(String::from(req_body)).await;
+    }
 
     // Ok(())
 }
